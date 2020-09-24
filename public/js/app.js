567a38d649fc0e667e5b443d8dabba67abac9c75
jimersylee <jimersylee@gmail.com>
2020-09-24 15:32:40 +0800

    feat(test): run the tests,set config

--- public/js/app.js
+++ public/js/app.js
@@ -73343,8 +73343,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Code/larazhihu/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Code/larazhihu/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/jimersylee/projects/docker/dnmp-plus/www/laravel-testing/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/jimersylee/projects/docker/dnmp-plus/www/laravel-testing/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
